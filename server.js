1b1374d9986ced20555970b8bd8a8e46910cc9c1
wenhao <wh.ho@outlook.com>
2017-03-10 18:50:12 +0800

    init

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-var http = require("http");
+var express = require("express");
+var app = express();
 
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(8000);
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!')
+})
 
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8000/');
