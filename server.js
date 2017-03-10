17cbff0ceaefb833a1eb60c5eb20b8db1925717c
wenhao <wh.ho@outlook.com>
2017-03-10 16:30:32 +0800

    port 8000

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    
    // Send the response body as "Hello World"
    response.end('Hello World\n');
-}).listen(15001);
+}).listen(8000);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:8000/');
